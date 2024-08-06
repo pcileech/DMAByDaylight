40868896f6c4394314227ac7b72130a0104d2480
pcileech <mikehass78@gmail.com>
2024-08-06 10:41:26 -0400

    Delete DMAByDaylight directory

--- DMAByDaylight/GeneratorEsp.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-extern std::shared_ptr<CheatFunction> UpdateGenerators;
-extern void DrawGeneratorEsp();
--- DMAByDaylight/SDK/Camera.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Engine.h"
-
-class Camera
-{
-public:
-	static Vector2 WorldToScreen(MinimalViewInfo viewinfo, Vector3 world);
-};
